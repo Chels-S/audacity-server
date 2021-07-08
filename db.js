2fb00edd6100132e1aa1861ff4d79a62eeb5b9f3
chels_s <clsextonc734@gmail.com>
2021-07-08 09:59:02 -0400

    updated db.js for heroku deployment

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 require ('dotenv').config();
-
 const {Sequelize} = require ('sequelize');
-const db = new Sequelize(process.env.DB_CONNECTION_STRING);
 
-module.exports = db;
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    ssl: process.env.ENVIRONMENT === 'production'
+})
+
+// const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+
+module.exports = sequelize;
